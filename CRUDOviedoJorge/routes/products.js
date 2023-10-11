729469f287b1b19bc77ed8416cef5ae64ed9ee15
Jorge Oviedo <jorgeos@gmail.com>
2023-10-11 14:01:19 -0400

    changed the database from the test database to the Marketplace db

--- CRUDOviedoJorge/routes/products.js
+++ CRUDOviedoJorge/routes/products.js
@@ -11,7 +11,7 @@ router
   .delete('/:id', productController.delete)
   .delete('/', productController.remover)
 
-// Handle a GET request
+// Handle a GET request, keep the req, and res, send them to the respective route
 router.get('/', (req, res) => {
   // Check if the 'name' query parameter is present in the request
   if (req.query.name) {
@@ -24,63 +24,3 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
-
-
-
-/**
-const ProductModel = require('../models/product.model');
-
-// Create a new product
-module.exports.create = async (req, res) => {
-  try {
-    const product = new ProductModel(req.body);
-    const result = await product.save();
-    res.json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Get all products
-module.exports.getAll = async (req, res) => {
-  try {
-    const products = await ProductModel.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Get a specific product by ID
-module.exports.getById = async (req, res) => {
-  try {
-    const product = await ProductModel.findById(req.params.id);
-    res.json(product);
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-
-// Update a product by ID
-module.exports.updateById = async (req, res) => {
-  try {
-    const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.json(product);
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-
-// Delete a product by ID
-module.exports.deleteById = async (req, res) => {
-  try {
-    await ProductModel.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Product deleted successfully' });
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-*/
