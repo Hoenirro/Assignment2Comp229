40c953495cb64d4c4ada1dd6a175ac96e4e4c6a2
Jorge Oviedo <jorgeos@gmail.com>
2023-10-11 12:33:08 -0400

    complete all the CRUD operations

--- CRUDOviedoJorge/controllers/product.controller.js
+++ CRUDOviedoJorge/controllers/product.controller.js
@@ -32,61 +32,7 @@ module.exports.remover = async ({}, res) => {
 }
 
 module.exports.findbyContent = async (req, res) => {
-  let result = await ProductModel.find({ name: { $regex: new RegExp(req.params.name, 'i') } })
-  res.json(req.params)
+  let result = await ProductModel.find({ name: { $regex: new RegExp(req.query.name, 'i') } })
+  res.json(result)
 }
 
-
-/**
-let ProductModel = require('../models/product.model');
-
-// Create a new product
-module.exports.create = async (req, res) => {
-    const product = new ProductModel(req.body);
-    let result = await product.save();
-    res.json(result);
-};
-
-// Get all products
-module.exports.getAll = async (req, res) => {
-  try {
-    const products = await ProductModel.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Get a specific product by ID
-module.exports.getById = async (req, res) => {
-  try {
-    const product = await ProductModel.findById(req.params.id);
-    res.json(product);
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-
-// Update a product by ID
-module.exports.updateById = async (req, res) => {
-  try {
-    const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.json(product);
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-
-// Delete a product by ID
-module.exports.deleteById = async (req, res) => {
-  try {
-    await ProductModel.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Product deleted successfully' });
-  } catch (err) {
-    res.status(404).json({ error: 'Product not found' });
-  }
-};
-*/
--- CRUDOviedoJorge/routes/products.js
+++ CRUDOviedoJorge/routes/products.js
@@ -5,12 +5,23 @@ var productController = require('../controllers/product.controller')
 /* GET users listing. */
 router
   .get('/:id', productController.findBID)
-  .get('/', productController.findAll)
+  //.get('/', productController.findAll)
   .post('/', productController.save)
   .put('/:id', productController.update)
   .delete('/:id', productController.delete)
   .delete('/', productController.remover)
-  .get('/', productController.findbyContent);
+
+// Handle a GET request
+router.get('/', (req, res) => {
+  // Check if the 'name' query parameter is present in the request
+  if (req.query.name) {
+    // If 'name' is present, use the 'findbyContent' function
+    productController.findbyContent(req, res);
+  } else {
+    // If 'name' is not present
+    productController.findAll(req, res);
+  }
+});
 
 module.exports = router;
 
